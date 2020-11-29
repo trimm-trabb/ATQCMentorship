8171e827309891d2852d7690c8c231ff85b633a5
trimm-trabb <anastasia.khartynova@gmail.com>
2020-11-29 15:57:02 +0200

    Created class to work with JSON convertion

---
+++ src/main/java/com/java/basics/JsonConvertion.java
@@ -0,0 +1,33 @@
+package com.java.basics;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonConvertion {
+
+    private static Person parseWithGson(String jsonString) {
+        Gson gson = new Gson();
+        Person p = gson.fromJson(jsonString, Person.class);
+        return p;
+    }
+
+    private static Person parseWithJackson(String jsonString) {
+        ObjectMapper mapper = new ObjectMapper();
+        Person p = null;
+        try {
+            p = mapper.readValue(jsonString, Person.class);
+        } catch (JsonProcessingException e) {
+            System.out.println("JsonProcessingException is caught");
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        String jsonString = "{ \"name\":\"John\", \"age\":30, \"car\":null }";
+        Person p1 = parseWithGson(jsonString);
+        System.out.println(p1.getName() + " " + p1.getAge());
+        Person p2 = parseWithJackson(jsonString);
+        System.out.println(p2.getName() + " " + p2.getAge());
+    }
+}
