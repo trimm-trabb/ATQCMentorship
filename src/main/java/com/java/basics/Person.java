ec4ac8262fbf6f11648b4fbde7a7339f75a6d07e
trimm-trabb <anastasia.khartynova@gmail.com>
2020-11-29 15:55:46 +0200

    Created class Person used later for JSON convertion and serialization

---
+++ src/main/java/com/java/basics/Person.java
@@ -0,0 +1,47 @@
+package com.java.basics;
+
+import java.io.Serializable;
+
+public class Person implements java.io.Serializable {
+
+    private String name;
+    private transient int age;
+    private String car;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String car) {
+        this.name = name;
+        this.age = age;
+        this.car = car;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getCar() {
+        return this.car;
+    }
+
+    public String toString() {
+        return this.getName() + " " + this.getAge() + " " + this.getCar();
+    }
+}
