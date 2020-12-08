5e9de6c4e39f2823905b37e42e66d57601d4f0c7
trimm-trabb <anastasia.khartynova@gmail.com>
2020-12-08 11:39:26 +0200

    Created classes to illustrate constructor overloading

---
+++ src/main/java/com/java/oop/constructorsdemo/ClassB.java
@@ -0,0 +1,7 @@
+package com.java.oop.constructorsdemo;
+
+public class ClassB extends ClassA {
+
+    public ClassB() {
+    }
+}
---
+++ src/main/java/com/java/oop/constructorsdemo/ConstructorTester.java
@@ -0,0 +1,9 @@
+package com.java.oop.constructorsdemo;
+
+public class ConstructorTester {
+
+    public static void main(String[] args) {
+        ClassA testObject = new ClassB();
+        System.out.println(testObject.toString());
+    }
+}
